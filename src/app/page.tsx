cdf032bb0e8853c36be0226aa2d18262c518db4d
unknown <dimiendrixmmiranda@gmail.com>
2025-02-17 16:47:23 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,60 @@
+'use client'
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	const listaDeProjetos = [
+		{
+			nome: 'Gamerzone',
+			imagem: '/logo/logo-gamer-zone.png',
+			link: 'https://gamerzone-nu.vercel.app/'
+		},
+		{
+			nome: 'AutoEscola',
+			imagem: '/logo/logo-autoescola.png',
+			link: '/https://autoescola-nine.vercel.app/'
+		},
+		{
+			nome: 'Prefeitura Joaquim Távora',
+			imagem: '/logo/logo-prefeitura.png',
+			link: 'https://prefeitura-municipal-joaquim-tavora-pr.vercel.app/'
+		},
+		{
+			nome: 'Quiz - Perguntas e Respostas',
+			imagem: '/logo/logo-quiz.png',
+			link: 'https://quiz-gold-beta.vercel.app/'
+		},
+		{
+			nome: 'Lista de Tarefas',
+			imagem: '/logo/logo-lista-de-tarefas.png',
+			link: 'https://lista-de-tarefas-phi-sage.vercel.app/'
+		},
+	]
+	return (
+		<div className="w-full min-h-screen bg-zinc-900 flex flex-col">
+			<div className="flex flex-col gap-4 p-3">
+				<h2 className="text-2xl font-bold text-center">Lista de Projetos</h2>
+				<ul className="flex flex-col gap-2">
+					{
+						listaDeProjetos.map((projeto, i) => {
+							return (
+								<li className="bg-black w-full h-full p-2 rounded-lg" key={i}>
+									<Link href={projeto.link} className="grid items-center gap-2" style={{ gridTemplateColumns: '50px 1fr' }}>
+										<div className="relative w-12 h-12">
+											<Image alt={projeto.nome} src={projeto.imagem} fill className="object-contain"></Image>
+										</div>
+										<h2 className="font-bold text-2xl whitespace-nowrap truncate">{projeto.nome}</h2>
+									</Link>
+								</li>
+							)
+						})
+					}
+				</ul>
+			</div>
+			<div className="mt-auto bg-black p-3 italic">
+				<p>Lista de Projetos desenvolvido por Dimi Endrix Martins Miranda</p>
+			</div>
+		</div>
+	);
 }
